e0b7f11b05da387c39a250d6c7d7b48069a7f7f8
Alexander Wiedenbruch <Alexander.Wiedenbruch@crealogix.com>
2021-02-26 21:14:58 +0100

    Initial commit

---
+++ src/main/java/de/example/Application.java
@@ -0,0 +1,10 @@
+package de.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
