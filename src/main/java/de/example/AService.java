d54db03a89bbe53ac87a747b95c8a7987f019f5b
awie <wirr01@gmail.com>
2021-02-26 21:43:31 +0100

    Service

---
+++ src/main/java/de/example/AService.java
@@ -0,0 +1,13 @@
+package de.example;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AService {
+    @RequestMapping("/service/{name}")
+    public String index(@PathVariable("name") String name) {
+        return "Greetings from Spring Boot, " + name + "!";
+    }
+}
